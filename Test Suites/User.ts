67e0fdc0e5b33d6b973a4ff15002bcc5deffeb68
erobertolima121 <e.robertolima121@gmail.com>
2019-09-13 11:05:48 -0300

    initial commit

---
+++ Test Suites/User.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>User</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7be11bfa-6659-4530-bf7e-9f1155ba06b5</testSuiteGuid>
+   <testCaseLink>
+      <guid>3233758c-d0dd-4949-8d89-327b73f926ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUSers</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
